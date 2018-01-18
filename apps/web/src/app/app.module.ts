f317e61fefb8745a27c7f4607ca5fee9d7243263
Piotr Kaczmarek <piotrkaczmarek1991@gmail.com>
2018-01-18 23:10:57 +0100

    Connect parties lib to web app

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NxModule } from '@nrwl/nx';
-import { RouterModule } from '@angular/router';
-import { partiesRoutes } from '@sonder-workspace/parties';
+import { RouterModule, Route } from '@angular/router';
+import { partiesRoutes, PartiesModule } from '@sonder-workspace/parties';
+
+const routes: Route[] = [
+  { path: "", pathMatch: "full", redirectTo: "parties" },
+  { path: "parties", children: partiesRoutes }
+];
+
 
 @NgModule({
   imports: [
   BrowserModule,
   NxModule.forRoot(),
-  RouterModule.forRoot([{path: 'parties', children: partiesRoutes}], {initialNavigation: 'enabled'})],
+  RouterModule.forRoot(routes, {initialNavigation: 'enabled'}),
+  PartiesModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
--- libs/parties/src/parties.module.ts
+++ libs/parties/src/parties.module.ts
@@ -3,7 +3,10 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
 import { SuggestedPartiesComponent } from './suggested-parties/suggested-parties.component';
 
-export const partiesRoutes: Route[] = [];
+export const partiesRoutes: Route[] = [
+  { path: "", pathMatch: "full", redirectTo: "suggested" },
+  { path: "suggested", component: SuggestedPartiesComponent }
+];
 
 @NgModule({
   imports: [CommonModule, RouterModule],
