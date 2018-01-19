00feb56aaf79426ea005dc4946341bb3de4a592d
Piotr Kaczmarek <piotrkaczmarek1991@gmail.com>
2018-01-19 22:30:52 +0100

    CLI: ng generate ngrx app --module=apps/web/src/app/app.module.ts  --root

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -4,6 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NxModule } from '@nrwl/nx';
 import { RouterModule, Route } from '@angular/router';
 import { partiesRoutes, PartiesModule } from '@sonder-workspace/parties';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { appReducer } from './+state/app.reducer';
+import { appInitialState } from './+state/app.init';
+import { AppEffects } from './+state/app.effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 const routes: Route[] = [
   { path: "", pathMatch: "full", redirectTo: "parties" },
@@ -16,8 +24,13 @@ const routes: Route[] = [
   BrowserModule,
   NxModule.forRoot(),
   RouterModule.forRoot(routes, {initialNavigation: 'enabled'}),
-  PartiesModule],
+  PartiesModule,
+  StoreModule.forRoot({app: appReducer}, {initialState: {app: appInitialState}}),
+  EffectsModule.forRoot([AppEffects]),
+  !environment.production ? StoreDevtoolsModule.instrument() : [],
+  StoreRouterConnectingModule],
   declarations: [AppComponent],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [AppEffects]
 })
 export class AppModule { }
