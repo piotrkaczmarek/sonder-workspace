d5c1e478a990fe91334d5cab3f476e22ee974b41
Piotr Kaczmarek <piotrkaczmarek1991@gmail.com>
2018-01-23 19:05:14 +0100

    CLI: ng generate component loginPage --app=auth

--- libs/auth/src/auth.module.ts
+++ libs/auth/src/auth.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 export const authRoutes: Route[] = [];
 
 @NgModule({
-  imports: [CommonModule, RouterModule]
+  imports: [CommonModule, RouterModule],
+  declarations: [LoginPageComponent]
 })
 export class AuthModule {
 }
