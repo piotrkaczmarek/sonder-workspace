7c2300877770998de76f4a7cabdd13a9145a19c1
Piotr Kaczmarek <piotrkaczmarek1991@gmail.com>
2018-01-23 21:35:05 +0100

    CLI: ng generate ngrx auth --module=libs/auth/src/auth.module.ts

--- libs/auth/src/auth.module.ts
+++ libs/auth/src/auth.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Route } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { authReducer } from './+state/auth.reducer';
+import { authInitialState } from './+state/auth.init';
+import { AuthEffects } from './+state/auth.effects';
 
 export const authRoutes: Route[] = [
   {
@@ -12,8 +17,9 @@ export const authRoutes: Route[] = [
 ];
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
-  declarations: [LoginPageComponent]
+  imports: [CommonModule, RouterModule, StoreModule.forFeature('auth', authReducer, {initialState: authInitialState}), EffectsModule.forFeature([AuthEffects])],
+  declarations: [LoginPageComponent],
+  providers: [AuthEffects]
 })
 export class AuthModule {
 }
