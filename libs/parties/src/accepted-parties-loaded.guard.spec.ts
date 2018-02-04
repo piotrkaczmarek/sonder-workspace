f0794701fd209bd21716e107146fed10b44820b3
Piotr Kaczmarek <piotrkaczmarek1991@gmail.com>
2018-02-04 10:47:23 +0100

    CLI: ng g guard accepted-parties-loaded --app=parties

---
+++ libs/parties/src/accepted-parties-loaded.guard.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed, async, inject } from '@angular/core/testing';
+
+import { AcceptedPartiesLoadedGuard } from './accepted-parties-loaded.guard';
+
+describe('AcceptedPartiesLoadedGuard', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [AcceptedPartiesLoadedGuard]
+    });
+  });
+
+  it('should ...', inject([AcceptedPartiesLoadedGuard], (guard: AcceptedPartiesLoadedGuard) => {
+    expect(guard).toBeTruthy();
+  }));
+});
