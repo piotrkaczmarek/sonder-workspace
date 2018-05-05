e5945baf11a6a47d4e974a50614ed004c65970aa
Piotr Kaczmarek <piotrkaczmarek1991@gmail.com>
2018-05-05 10:30:54 +0200

    Comment out unused specs

--- libs/posts/src/components/comment-tree/comment-item/comment-item.component.spec.ts
+++ libs/posts/src/components/comment-tree/comment-item/comment-item.component.spec.ts
@@ -1,25 +1,25 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CommentItemComponent } from './comment-item.component';
+// import { CommentItemComponent } from './comment-item.component';
 
-describe('CommentItemComponent', () => {
-  let component: CommentItemComponent;
-  let fixture: ComponentFixture<CommentItemComponent>;
+// describe('CommentItemComponent', () => {
+//   let component: CommentItemComponent;
+//   let fixture: ComponentFixture<CommentItemComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CommentItemComponent ]
-    })
-    .compileComponents();
-  }));
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ CommentItemComponent ]
+//     })
+//     .compileComponents();
+//   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CommentItemComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(CommentItemComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
--- libs/posts/src/components/comment-tree/comment-tree.component.spec.ts
+++ libs/posts/src/components/comment-tree/comment-tree.component.spec.ts
@@ -1,25 +1,25 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CommentTreeComponent } from './comment-tree.component';
+// import { CommentTreeComponent } from './comment-tree.component';
 
-describe('CommentTreeComponent', () => {
-  let component: CommentTreeComponent;
-  let fixture: ComponentFixture<CommentTreeComponent>;
+// describe('CommentTreeComponent', () => {
+//   let component: CommentTreeComponent;
+//   let fixture: ComponentFixture<CommentTreeComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CommentTreeComponent ]
-    })
-    .compileComponents();
-  }));
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ CommentTreeComponent ]
+//     })
+//     .compileComponents();
+//   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CommentTreeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(CommentTreeComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
